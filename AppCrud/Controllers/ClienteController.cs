7df94ba9f2f8e4bd11247c9ef3b8f1949a5cda64
jfelipeq14 <juan_fquintero@soy.sena.edu.co>
2024-10-30 11:47:24 -0500

    Add controller with list by context

---
+++ AppCrud/Controllers/ClienteController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AppCrud.Data;
+using AppCrud.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppCrud.Controllers
+{
+    public class ClienteController : Controller
+    {
+        private readonly AppDBContext _context;
+
+        public ClienteController(AppDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Lista()
+        {
+            List<Cliente> listaClientes = await _context.Clientes.ToListAsync();
+            return View(listaClientes);
+        }
+    }
+}
