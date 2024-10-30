c7877ea941fd44c88037aa8bd2d5fab3d67796f1
jfelipeq14 <juan_fquintero@soy.sena.edu.co>
2024-10-30 10:24:43 -0500

    Add project files.

---
+++ AppCrud/Models/Cliente.cs
@@ -0,0 +1,15 @@
+﻿namespace AppCrud.Models
+{
+    public class Cliente
+    {
+        public int IdEmpleado { get; set; }
+        public int IdUsuario { get; set; }
+        public int IdMunicipio { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public string Sexo { get; set; }
+        public string TipoDeSangre { get; set; }
+        public string Eps { get; set; }
+        public bool Estado { get; set; }
+    }
+}
